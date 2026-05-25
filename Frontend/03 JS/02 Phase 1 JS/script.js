b596c2040a2a3f147493a748bf18d820cad7a161
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-05-26 00:09:54 +0530

    Day 2 of Phase 1 of js

---
+++ Frontend/03 JS/02 Phase 1 JS/script.js
@@ -0,0 +1,158 @@
+// // Revision of previous class
+// console.log('Today is day 2 of phase 1 of JavaScript');
+// console.warn('this is warning')
+// console.error('this is the error')
+// var aa = 120n
+// console.log(aa);
+// console.log(typeof(aa));
+// console.table([
+//     {id: 301, user: 'Abhishek'},
+//     {id: 302, user: 'Mantu Tiwari'},
+//     {id: 303, user: 'Shivam'},
+// ])
+// // var bb = alert('hello')
+// // console.log(bb);
+// // var cc = confirm('are you sure')
+// // console.log(cc);
+// // var dd = prompt('what is your name ')
+// // console.log(dd);
+
+// // var x = prompt('enter the num 1')
+// // var y = prompt('enter the num 2')
+// // console.log(x+y); // Example of conversion 
+// // console.log(x-y); // Exapmple of cohesion
+// // console.log(x*y);
+
+// // cohesion
+// // var a1 = prompt('num1')
+// // var a2 = prompt ('num2')
+// // var b1 = Number(a1)
+// // console.log(a1); // string
+// // console.log(b1); // number
+// // var b2 = Number(a2)
+// // console.log(a1+a2);
+// // console.log(b1+b2);
+// // console.log(typeof(a1));
+// // console.log(typeof(b1));
+
+// // var a = '20'
+// // var b = Number(a)
+// // console.log(a); //string
+// // console.log(b); //number
+
+// // var a = Number(prompt('Enter num 1'))
+// // var b = Number(prompt('Enter num 2'))
+// // // var c = Number(a)
+// // // var d = Number(b)
+// // console.log(a+b);
+// // // console.log(c+d);
+
+
+// //  pre increment and post increment
+// var a = 10
+// console.log(a); 
+// ++a
+// console.log(a); 
+// console.log(a); 
+// ++a
+// console.log(a); 
+// console.log(++a);
+// console.log(a++); 
+// console.log(a++); 
+// console.log(a); 
+// console.log(--a);
+// console.log(a--);
+// a--
+// --a
+// console.log(a);
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+
+// //  Logical Operator
+// var p = 10
+// var q = 20
+// var r = 30
+// var s = 40
+// console.log(0 !== '0' );
+// console.log(0 != '0' );
+// console.log(0 === '0' );
+// console.log(0 == '0' );
+// console.log(p<q);
+// console.log(p>q || r<s);
+// console.log(p>q && r<s);
+// console.log(p<q && r<s);
+
+// Conditional Statement
+var a = 20
+var b = '20'
+if (a > b){
+    console.log('nacho');
+}
+else{
+    console.log('mat nacho'); 
+}
+if (a == b){
+    console.log('I love JS');
+}
+else{
+    console.log('I hate JS');
+}
+if (a === b){
+    console.log('I love JS');
+}
+else{
+    console.log('I hate JS');
+}
+
+// // Example 2
+// var marks = Number(prompt('Enter you marks in %'))
+// if (marks >= 85){
+//     console.log('Boom! you are eligible for Scholarship');
+// }else{
+//     console.log('you are not eligible');
+// }
+
+// //Example 2 advance
+// var maths = Number(prompt('Enter Maths Marks:'))
+// var phy = Number(prompt('Enter Physics Marks:'))
+// var che = Number(prompt('Enter Chemistry Marks:'))
+// var avg = (maths+phy+che)/3
+// if(avg >= 85){
+//     console.log('Eligible for Scholorship');
+// }else{
+//     console.log('not eligible');
+// }
+
+// // Example 3
+// var gender = prompt('Enter your Gender (M/F)')
+// var age = Number(prompt('Enter the age:'))
+// if (gender == 'F' || 'ff'){
+//     if (age >= 18 && age <= 60){
+//         console.log('Eligible');
+//     }else{
+//         console.log('not Eligible');
+//     }
+// }else{
+//     console.log('Not Avaialbe')
+// }
+
+// ternary operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
