36a18f5113712c1be5878fc6be50016f3d13c221
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-08 02:45:38 +0530

    js phase 4

--- Frontend/03 JS/09 Phase 4 Object Deeper/script.js
+++ Frontend/03 JS/09 Phase 4 Object Deeper/script.js
@@ -55,3 +55,107 @@ var nestedMethod = {
     }
 }
 nestedMethod.college()
+//
+const obj3 = {
+    name: 'Mantu',
+    age: 20,
+    company: {
+        name: 'sheryians',
+        course: 'cohort',
+        duration: 2,
+        getName:  function() {
+            console.log(this.name);
+        }
+    }
+} 
+obj3.company.getName()
+//
+const obj4 = {
+    name: 'Mantu',
+    age: 20,
+    getName: function () {
+        const companyName = {
+            name: 'sheryians',
+            isGood: true,
+            greet: () => {  // if you chage this to ananyoms function then it will return sheryians
+                console.log(this.name);
+            }
+        }
+        companyName.greet()
+    }
+} 
+obj4.getName()
+//
+const stu1 = {
+    firstName: 'Mantu',
+    lastName: 'Tiwari',
+    getName: function() {
+        console.log(this.firstName+''+this.lastName);
+    }
+}
+stu1.getName()
+
+// call (function sharing)
+let student1 = {
+    firstName: 'Rahul',
+    lastName: 'Gupta',
+    maths: 88,
+    science: 97,
+    sst: 67,
+}
+let student2 = {
+    firstName: 'kartik',
+    lastName: 'sharma',
+    maths: 98,
+    science: 77,
+    sst: 89,
+}
+let student3 = {
+    firstName: 'Mayank',
+    lastName: 'Tiwari',
+    maths: 79,
+    science: 83,
+    sst: 90,
+}
+const getMarks = function (city, state) {
+    console.log(`${this.firstName} ${this.lastName}, You got ${(this.maths+this.science+this.sst)/3}% and he is from ${city} (${state})`);
+}
+getMarks.call(student3,'Patna','BR') //here we use call to use other function to another function
+getMarks.apply(student2,['Mumbai','MH']) // apply me bas ham array me argument ko pass karte hai
+var out = getMarks.bind(student1,'Patna','BR')
+out() // bind is similar to call but the only difference is that it store into the variable
+// function me agar argument pass hoga to wo calll me comma se seprated hoga
+
+// ************************* Prototype
+const arr = [10,20,30,40]
+console.log(arr); // console pe arr print hoga
+console.log(arr.__proto__); // prototype show hoga in the form of array
+console.log(arr.__proto__.__proto__); // prototype show hoga in the form of object
+console.log(arr.__proto__.__proto__.__proto__); // last point null show karega
+console.log('individual prototype');
+console.log(Array.prototype);
+console.log(Object.prototype);
+console.log(Function.prototype);
+console.log(Object.prototype === Array.prototype.__proto__);
+console.log(Object.prototype === Function.prototype.__proto__);
+console.log(Object.prototype.__proto__ === Function.prototype.__proto__.__proto__);
+console.log(Object.prototype.__proto__);
+
+// prototypical inheritance
+const company = {
+    name: 'Sheryians',
+    foundYear: 2022,
+    slogan: function() {
+        console.log('We teach what Matters');
+    }
+}
+const user = {
+    name : 'Mantu',
+    age: 22,
+}
+user.__proto__ = company // don't do this because this will change the default prototype to assigned prototype which make the performance issue
+console.log(user);
+console.log(user.foundYear); // it inherit the property of company object 
+
+
+
