361be60f12527f2d5db317da08a46de64ed584aa
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-21 03:19:06 +0530

    auto reset added

--- Frontend/04 DOM/05 DOM Event Delegation/script.js
+++ Frontend/04 DOM/05 DOM Event Delegation/script.js
@@ -13,7 +13,6 @@ const btn = document.querySelector("button");
 const t = document.querySelector(".time");
 const s = document.querySelector(".score");
 const overlay = document.querySelector("#overlay");
-// 1. Create the box once and add it to the page immediately, but keep it hidden
 const box = document.createElement("div");
 box.classList.add("box");
 box.style.display = "none"; // Hide it until the game starts
@@ -38,17 +37,24 @@ btn.addEventListener("click", () => {
   time = 0;
   score = 0;
   s.innerHTML = `<p class="score">Score : ${score}</p>`;
-  box.style.display = "block"; // Make the box visible when game starts
-  randomBox();
+  box.style.display = "block";
   move = setInterval(() => {
     box.style.backgroundColor = randomColor();
     randomBox();
   }, 1000);
   setTimeout(() => {
     clearInterval(move);
-    box.style.display = "none"; // Hide the box when game ends
+    box.style.display = "none";
     overlay.style.display = "flex";
     console.log("stopped");
+    setTimeout(() => {
+      console.log("overlay disapper");
+      overlay.style.display = "none";
+      score = 0;
+      time = 0;
+      t.innerHTML = `<p>Time : ${time} sec </p>`;
+       s.innerHTML = `<p class="score">Score : ${score}</p>`;
+    }, 3000);
   }, 10000);
 });
 box.addEventListener("click", () => {
