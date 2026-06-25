2b3d1816119da1215465f3ef310d6148b59f0427
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-26 00:02:05 +0530

    close task

--- Frontend/04 DOM/A1 Task Manager/script.js
+++ Frontend/04 DOM/A1 Task Manager/script.js
@@ -1,7 +1,11 @@
 const createBtn = document.querySelector('.create-btn')
 const formClass = document.querySelector('.form')
+const close = document.querySelector('.ri-close-circle-line')
 
 createBtn.addEventListener('click',() => {
     formClass.style.display = 'flex'
 })
 
+close.addEventListener('click', ()=> {
+    formClass.style.display = 'none'
+})
