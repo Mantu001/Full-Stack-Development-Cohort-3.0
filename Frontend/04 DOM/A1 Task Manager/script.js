2d094b7cb1faaa65cc5c9e9373be2d5132540107
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-06-26 09:29:23 +0530

    dark mode added

--- Frontend/04 DOM/A1 Task Manager/script.js
+++ Frontend/04 DOM/A1 Task Manager/script.js
@@ -3,7 +3,9 @@ const main = document.querySelector('main')
 const createBtn = document.querySelector('.create-btn')
 const formClass = document.querySelector('.form')
 const close = document.querySelector('.ri-close-circle-line')
-const darkMode = document.querySelector('.ri-moon-fill')
+const darkMode = document.querySelector('.dark-mode')
+const taskContainer = document.querySelector('.task-container')
+const nav = document.querySelector('nav')
 
 createBtn.addEventListener('click',() => {
     formClass.style.display = 'flex'
@@ -12,6 +14,15 @@ createBtn.addEventListener('click',() => {
 close.addEventListener('click', ()=> {
     formClass.style.display = 'none'
 })
+
 darkMode.addEventListener('click',() => {
-    console.log('click');
+    if (main.classList.toggle('dark')){
+        taskContainer.style.backgroundColor = 'grey'
+        nav.style.backgroundColor = 'grey'
+        nav.style.color = 'white'
+    }else{
+        taskContainer.style.backgroundColor = 'white'
+        nav.style.backgroundColor = '#BFDDF0'
+        nav.style.color = '#003edb'
+    }
 })
