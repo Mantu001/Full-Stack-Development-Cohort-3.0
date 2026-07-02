770af03545ab00ac4c8b9b6c796cd8d7a4144dee
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-02 07:06:53 +0530

    component created

--- Frontend/05 REACT JS/04 Props and State/src/App.jsx
+++ Frontend/05 REACT JS/04 Props and State/src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import noName, {one, two} from './test'
+// import React from 'react'
+// // import noName, {one, two} from './test'
+// const App = () => {
+//   // noName()
+//   // one()
+//   // two()
+//   return (
+//     <div>
+//       <h1>Boiler Plate of React is rafce</h1>
+//     </div>
+//   )
+// }
+// export default App
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 const App = () => {
-
-  noName()
-  one()
-  two()
   return (
     <div>
-      <h1>Boiler Plate of React is rafce</h1>
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
---
+++ Frontend/05 REACT JS/04 Props and State/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "cyan",
+        fontFamily: "roboto",
+        fontSize: "10px",
+      }}
+    >
+      <h1>This is Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
