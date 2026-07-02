d7cfba4556e505399dbdc71b94c137cad9fc0185
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-02 12:57:51 +0530

    bool example

--- Frontend/05 REACT JS/04 Props and State/src/App.jsx
+++ Frontend/05 REACT JS/04 Props and State/src/App.jsx
@@ -12,30 +12,46 @@
 // }
 // export default App
 
+// // counter function
+// import React, { useState } from "react";
+// // import Navbar from "./components/Navbar";
+// // import Hero from "./components/Hero";
+// // import Footer from "./components/Footer";
+// const App = () => {
+//   let [count, setCount] = useState(0);
+//   // console.log('hello');
+//   return (
+//     <div>
+//       <h1>Count : {count}</h1>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//           // count++;
+//           // console.log(count);
+//         }}
+//       >
+//         Click to Increase
+//       </button>
+//       {/* <Navbar />
+//       <Hero />
+//       <Footer /> */}
+//     </div>
+//   );
+// };
+// export default App;
 
-// counter function
+
+// Counter other example
 import React, { useState } from "react";
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import Footer from "./components/Footer";
 const App = () => {
-  let [count, setCount] = useState(0);
-  // console.log('hello');
+  let [rev, setRev] = useState(true);
+  console.log(rev);
   return (
     <div>
-      <h1>Count : {count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-          // count++;
-          // console.log(count);
-        }}
-      >
-        Click to Increase
-      </button>
-      {/* <Navbar />
-      <Hero />
-      <Footer /> */}
+      <h1>value : true</h1>
+      <button onClick={() => {
+        setRev(false)
+      }}>Change</button>
     </div>
   );
 };
